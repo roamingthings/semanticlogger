3324a7420dcc94c2615287f5e5bb164cb642e1f4
Alexander Sparkowsky <info@roamingthings.de>
2018-12-01 19:40:00 +0100

    Update build to use maven-publish plugin for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.api.JavaVersion.VERSION_1_8
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.time.Year
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
@@ -14,32 +15,17 @@ buildscript {
 }
 
 plugins {
-    kotlin("jvm") version "1.3.10"
     `java-library`
+    `maven-publish`
+    signing
+    kotlin("jvm") version "1.3.10"
     id("com.github.hierynomus.license") version "0.14.0"
-    id("com.bmuschko.nexus") version "2.3.1"
-}
-
-if (JavaVersion.current().isJava9Compatible) {
-    tasks.withType<GroovyCompile>().configureEach { options.compilerArgs.addAll(arrayOf("--release", "8")) }
-}
-
-if (JavaVersion.current().isJava8Compatible) {
-    tasks.withType<GroovyCompile>().configureEach { options.compilerArgs.addAll(arrayOf("Xdoclint:none", "-quiet")) }
-}
-
-gradle.taskGraph.whenReady {
-    if (hasTask(":publishPlugins")) {
-        check("git diff --quiet --exit-code".execute(null, rootDir).waitFor() == 0) { "Working tree is dirty" }
-        val process = "git describe --exact-match".execute(null, rootDir)
-        check(process.waitFor() == 0) { "Version is not tagged" }
-        version = process.text.trim().removePrefix("v")
-    }
 }
 
 group = "de.roamingthings"
 
 repositories {
+    mavenCentral()
     jcenter()
 }
 
@@ -62,16 +48,10 @@ dependencies {
     testCompile("org.mockito:mockito-all:${extra["mockitoVersion"]}")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
-
+/***
+ * Test Configuration and Tasks
+ */
 tasks {
-    withType<JavaCompile>().configureEach {
-        options.compilerArgs.addAll(arrayOf("-Xlint:all", "-Werror"))
-    }
-
     named<Test>("test") {
         useJUnitPlatform()
         testLogging {
@@ -85,6 +65,9 @@ tasks {
     }
 }
 
+/**
+ * Kotlin Configuration and Tasks
+ */
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
@@ -104,7 +87,7 @@ tasks {
         args("**/*.gradle.kts", "**/*.kt")
     }
 
-    named<Task>("check") {
+    named<Task>("compileKotlin") {
         dependsOn(verifyKtlint)
     }
 
@@ -116,6 +99,31 @@ tasks {
     }
 }
 
+/***
+ * Java Configuration and Tasks
+ */
+if (JavaVersion.current().isJava8Compatible) {
+    tasks.withType<GroovyCompile>().configureEach { options.compilerArgs.addAll(arrayOf("Xdoclint:none", "-quiet")) }
+}
+
+if (JavaVersion.current().isJava9Compatible) {
+    tasks.withType<GroovyCompile>().configureEach { options.compilerArgs.addAll(arrayOf("--release", "8")) }
+}
+
+java {
+    sourceCompatibility = VERSION_1_8
+    targetCompatibility = VERSION_1_8
+}
+
+tasks {
+    withType<JavaCompile>().configureEach {
+        options.compilerArgs.addAll(arrayOf("-Xlint:all", "-Werror"))
+    }
+}
+
+/***
+ * License Handling
+ */
 license {
     header = rootProject.file("doc/APL.header")
     skipExistingHeaders = true
@@ -131,6 +139,95 @@ license {
     (this as ExtensionAware).extra["name"] = "Alexander Sparkowsky"
 }
 
+/**
+ * Distribution
+ */
+task<Jar>("sourcesJar") {
+    from(sourceSets["main"].allSource)
+    classifier = "sources"
+}
+
+task<Jar>("javadocJar") {
+    from(tasks["javadoc"])
+    classifier = "javadoc"
+}
+
+/**
+ * Publishing
+ */
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "semanticlogger"
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+            pom {
+                name.set("Semantic Logging")
+                description.set("A Library to add semantic logging to your JVM project using SLF4J")
+                url.set("https://github.com/roamingthings/semanticlogger")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("roamingthings")
+                        name.set("Alexander Sparkowsky")
+                        email.set("info@roamingthings.de")
+                    }
+                }
+                scm {
+                    connection.set("scm:git@github.com:roamingthings/semanticlogger.git")
+                    developerConnection.set("scm:git@github.com:roamingthings/semanticlogger.git")
+                    url.set("https://github.com/roamingthings/semanticlogger")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            authentication {
+                credentials {
+                    username =
+                            (if (project.hasProperty("nexusUsername"))
+                                project.properties["nexusUsername"]
+                            else
+                                System.getenv("nexusUsername")) as String
+                    password =
+                            (if (project.hasProperty("nexusPassword"))
+                                project.properties["nexusPassword"]
+                            else
+                                System.getenv("nexusPassword")) as String
+                }
+            }
+        }
+    }
+}
+
+tasks.withType<Sign> {
+    onlyIf { project.hasProperty("signing.keyId") }
+}
+
+signing {
+    useGpgCmd()
+    isRequired = false
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks {
+    named<Javadoc>("javadoc") {
+        if (JavaVersion.current().isJava9Compatible) {
+            (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+        }
+    }
+}
+
 fun String.execute(envp: Array<String>?, workingDir: File?) =
     Runtime.getRuntime().exec(this, envp, workingDir)
 
@@ -138,4 +235,3 @@ val Process.text: String
     get() = inputStream.bufferedReader().readText()
 
 apply(from = "gradle/circleci.gradle.kts")
-apply(from = "gradle/publish.gradle")
